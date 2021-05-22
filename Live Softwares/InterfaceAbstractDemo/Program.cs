5792f3d19de9171ad8a6153ccd6e88f4c249fc31
Ümit Dengiz <umitdengiz98@gmail.com>
2021-05-22 17:08:28 +0300

    Demo

---
+++ Live Softwares/InterfaceAbstractDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Concrete;
+using InterfaceAbstractDemo.Entities;
+using System;
+
+namespace InterfaceAbstractDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            BaseCustomerManager baseCustomerManager = new StarbucksCustomerManager(new MernisServiceAdapter());
+            baseCustomerManager.Save(new Customer { DateOfBirth = new DateTime(1990, 1, 8), FirstName = "Engin", LastName = "Deniz", Id = 1, NationalityId = "99999999999" });
+
+
+            Console.ReadLine();
+        }
+    }
+}
